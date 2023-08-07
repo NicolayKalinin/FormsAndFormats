31c8c2ea9c245de840dca025d26c8d8e9a5b1162
Nicolay Kalinin <nikolajk202@gmail.com>
2023-08-07 18:24:44 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import server.Server;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+}
